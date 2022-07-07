68f89a224e2877c93e92284615c9bd9ad9be599a
Cr4zyEight <v.frebel@gmail.com>
2022-07-07 11:16:18 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,66 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import localeDe from '@angular/common/locales/de';
+import { registerLocaleData, DatePipe } from '@angular/common';
+import { FileSaverModule } from 'ngx-filesaver';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbSidebarModule,
+  NbButtonModule,
+  NbInputModule,
+  NbCardModule,
+  NbMenuModule,
+  NbWindowModule,
+  NbContextMenuModule,
+  NbUserModule,
+  NbIconModule,
+  NbPopoverModule,
+  NbDatepickerModule,
+  NbChatModule,
+  NbToastrModule,
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NbDateFnsDateModule } from '@nebular/date-fns';
+
+registerLocaleData(localeDe);
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    LayoutModule,
+    FileSaverModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbDateFnsDateModule.forRoot({ format: 'MM/dd/yyyy' }),
+    NbDatepickerModule.forRoot(),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbSidebarModule.forRoot(),
+    NbButtonModule,
+    NbInputModule,
+    NbCardModule,
+    NbChatModule,
+    NbMenuModule.forRoot(),
+    NbWindowModule.forRoot(),
+    NbContextMenuModule,
+    NbUserModule,
+    NbIconModule,
+    FontAwesomeModule,
+    NbPopoverModule,
+    NbToastrModule.forRoot(),
+  ],
+  schemas: [],
+  declarations: [AppComponent],
+  providers: [DatePipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
